f334e9decdd6a8401376e5fdbc2f4f8ea7e7d5b0
pengzhanbo <volodymyr@foxmail.com>
2023-09-09 15:54:10 +0800

    wip: music player controller

---
+++ src/features/MusicPlayer/MusicController/index.tsx
@@ -0,0 +1,31 @@
+import MusicControl from './MusicControl'
+import MusicExtra from './MusicExtra'
+import MusicInfo from './MusicInfo'
+import Progress from '~/components/Progress'
+import { usePlayer } from '~/hooks'
+
+function MusicController() {
+  const { playerState, seek } = usePlayer()
+  const { currentTime, song, buffered } = playerState
+  const total = song?.dt || 0
+  const changeCurrentTime = (percent: number) => {
+    seek((percent * total) / 1000)
+  }
+  return (
+    <div className="h-full w-full">
+      <Progress
+        total={total}
+        current={currentTime}
+        preload={buffered}
+        onChange={changeCurrentTime}
+      />
+      <div className="h-full flex-center">
+        <MusicInfo />
+        <MusicControl />
+        <MusicExtra />
+      </div>
+    </div>
+  )
+}
+
+export default MusicController
--- src/features/MusicPlayer/MusicPlayer.tsx
+++ src/features/MusicPlayer/MusicPlayer.tsx
@@ -1,31 +1,9 @@
-import MusicControl from './MusicControl'
-import MusicExtra from './MusicExtra'
-import MusicInfo from './MusicInfo'
-import Progress from '~/components/Progress'
-import { usePlayer } from '~/hooks'
+import MusicController from './MusicController'
 
-function MusicPlayer() {
-  const { playerState, seek } = usePlayer()
-  const { currentTime, song, buffered } = playerState
-  const total = song?.dt || 0
-  const changeCurrentTime = (percent: number) => {
-    seek((percent * total) / 1000)
-  }
+export function MusicPlayer() {
   return (
-    <div className="h-full w-full">
-      <Progress
-        total={total}
-        current={currentTime}
-        preload={buffered}
-        onChange={changeCurrentTime}
-      />
-      <div className="h-full flex-center">
-        <MusicInfo />
-        <MusicControl />
-        <MusicExtra />
-      </div>
-    </div>
+    <>
+      <MusicController />
+    </>
   )
 }
-
-export default MusicPlayer
